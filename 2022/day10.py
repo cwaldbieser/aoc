9dc9abbed771dec487b7c885c05e20d567c68552
Carl Waldbieser <cwaldbieser@gmail.com>
2022-12-10 12:04:32 -0500

    Solved 2022 day 10, part 1.
    Solved 2022 day 10, part 1.

---
+++ 2022/day10.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+import argparse
+import itertools
+
+instruction_map = {
+    "addx": ["noop", "addx"],
+    "noop": ["noop"],
+}
+
+
+def main(args):
+    """
+    The main function entrypoint.
+    """
+    sample_cycles = itertools.count(args.start_cycle, args.interval)
+    sample_total = 0
+    instructions = parse_input(args.infile)
+    registers = {"X": 1}
+    sample_cycle = next(sample_cycles)
+    for t0, (instruction, argument) in enumerate(instructions):
+        t = t0 + 1
+        print(
+            "t: {:5d}, instr: {}, arg: {:7d}, regX: {:7d}".format(
+                t, instruction, argument, registers["X"]
+            ),
+            end="",
+        )
+        if t == sample_cycle:
+            signal_strength = registers["X"] * t
+            sample_total += signal_strength
+            sample_cycle = next(sample_cycles)
+            print(" * signal strength: {:10d}".format(signal_strength))
+        else:
+            print("")
+        if instruction == "addx":
+            registers["X"] += argument
+    print("")
+    print("Total signal strength: {}".format(sample_total))
+
+
+def parse_input(infile):
+    """
+    Generator produces instructions from each line of input.
+    """
+    for line in infile:
+        line = line.strip()
+        parts = line.split()
+        instruction = parts[0]
+        argument = 0
+        if len(parts) > 1:
+            argument = int(parts[1])
+        instr_cycles = instruction_map[instruction]
+        for instr_code in instr_cycles:
+            yield instr_code, argument
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Advent of Code 2022, day 10")
+    parser.add_argument(
+        "infile", type=argparse.FileType("r"), action="store", help="The input file."
+    )
+    parser.add_argument(
+        "--start-cycle",
+        type=int,
+        default=20,
+        help="The starting CPU cycle at which to sample signal strength.",
+    )
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=40,
+        help="The CPU cycle interval at which to sample signal strength.",
+    )
+    args = parser.parse_args()
+    main(args)
