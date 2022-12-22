39a6dd8cf3ec04d83fe1782a1e828f564579ad1a
Carl Waldbieser <cwaldbieser@gmail.com>
2022-12-22 12:19:33 -0500

    Aside from the fact my solver doesn't solve for the maximum geodes, it works perfectly!
    Sigh.  I think this is going to be another graph puzzle.  I don't care
    much for graph puzzles.

--- 2022/day19.py
+++ 2022/day19.py
@@ -11,33 +11,160 @@ def main(args):
     blueprints = parse_blueprints(args.infile)
     blueprints = dict(blueprints)
     for label, blueprint in blueprints.items():
-        print(label, blueprint)
+        print_heading("Blueprint", "=")
+        print_blueprint(label, blueprint)
+        evaluate_blueprint(blueprint)
+
+
+def print_heading(heading, symbol="-"):
+    """
+    Print a heading.
+    """
+    print(heading)
+    print(symbol * len(heading))
+    print("")
+
+
+def print_blueprint(label, blueprint):
+    """
+    Print a blueprint.
+    """
+    print("{}: ".format(label))
+    for resource in ["geode", "obsidian", "clay", "ore"]:
+        cost = blueprint.get(resource)
+        if cost is None:
+            continue
+        print("+ {} robot".format(resource))
+        for qty, res_type in cost:
+            print("  - {:2d} {}".format(qty, res_type))
+    print("")
+
+
+def evaluate_blueprint(blueprint):
+    """
+    Evaluate a blueprint.
+    """
     max_time = 24
-    robots = {
-        "ore": 1,
-    }
+    robots = collections.Counter()
+    robots["ore"] = 1
     resources = collections.Counter()
-    for minute in range(max_time):
-        print("= Minute {} =".format(minute))
-        harvest_resources(robots, resources)
+    print_heading("Max. Ratios")
+    for minute in range(1, max_time + 1):
+        heading = "Minute {}".format(minute)
+        print_heading(heading)
+        additional_resources = harvest_resources(robots, resources)
         print("")
-    print("= Resources =")
-    print(resources)
+        invest_resources(minute, blueprint, robots, resources, goal="geode")
+        print("")
+        resources = resources + additional_resources
+        print_heading("Resources", '"')
+        for resource_type, qty in resources.items():
+            print("- {}: {:4}".format(resource_type, qty))
+        print("")
+        print_heading("Robots", '"')
+        for robot_type, qty in robots.items():
+            print("- {}: {:3d}".format(robot_type, qty))
+        print("")
+
+
+def calculate_max_ratios(blueprint):
+    """
+    Calculate the maximum ratios between resource producing robots.
+    """
+    max_ratios = collections.defaultdict(lambda: collections.defaultdict(lambda: 0.0))
+    resources = ["geode", "obsidian", "clay", "ore"]
+    for resource in resources:
+        for costs in blueprint.values():
+            cost_map = dict((cost_type, qty) for qty, cost_type in costs)
+            res_qty = cost_map.get(resource)
+            if res_qty is None:
+                continue
+            ratios = collections.defaultdict(lambda: 0.0)
+            for cost_type, qty in cost_map.items():
+                if cost_type == resource:
+                    continue
+                ratios[cost_type] = res_qty / qty
+            if len(ratios) > 0:
+                max_ratios[resource] = ratios
+    return max_ratios
+
+
+def invest_resources(
+    minute,
+    blueprint,
+    robots,
+    resources,
+    goal,
+    not_achievable=None,
+    level=0,
+    explain_levels=None,
+):
+    """
+    Choose which resources to invest into which robots.
+    """
+    if explain_levels is None:
+        explain_levels = set({})
+    explain = False
+    if level in explain_levels:
+        explain = True
+    if explain:
+        print("- Goal: {}, level: {}".format(goal, level))
+    if not_achievable is None:
+        not_achievable = set({})
+    cost = blueprint[goal]
+    achievable_now = True
+    for qty_required, resource in cost:
+        on_hand = resources[resource]
+        if on_hand < qty_required:
+            achievable_now = False
+            if explain:
+                print("- Cannot achieve goal of creating {} robot.".format(goal))
+                print("- Not enough {} available.".format(resource))
+            if goal == resource:
+                not_achievable = not_achievable | {goal}
+            if resource in not_achievable:
+                if explain:
+                    print("Cannot achieve goal at this time.")
+                continue
+            if explain:
+                print("- Investing in {} ...".format(resource))
+            invest_resources(
+                minute,
+                blueprint,
+                robots,
+                resources,
+                goal=resource,
+                not_achievable=not_achievable | {goal},
+                level=level + 1,
+            )
+    if achievable_now:
+        print("- Creating {} robot.  Allocating resources.".format(goal))
+        for qty_required, resource in cost:
+            resources[resource] -= qty_required
+            print(
+                "- Used {} {}.  {} units of {} remain.".format(
+                    qty_required, resource, resources[resource], resource
+                )
+            )
+        robots[goal] += 1
 
 
 def harvest_resources(robots, resources):
     """
     Harvest resources and updated `resources`.
     """
+    additional_resources = collections.Counter()
     for resource, qty in robots.items():
-        resources[resource] += qty
-        print("Robots harvested {} units of {}.".format(qty, resource))
+        additional_resources[resource] += qty
+        print("- Robots harvested {} units of {}.".format(qty, resource))
+    return additional_resources
 
 
 def parse_blueprints(infile):
     """
     Generator yields blueprints from input file.
     """
+    resource_order = {"geode": 0, "obsidian": 1, "clay": 2, "ore": 3}
     for line in infile:
         line = line.strip()
         parts = line.split(": ")
@@ -55,8 +182,11 @@ def parse_blueprints(infile):
                     qty = int(word)
                 except ValueError:
                     continue
-                resources.add((qty, nextword))
-            conversions[robot_type] = resources
+                resource = nextword.replace(".", "")
+                resources.add((qty, resource))
+                ordered_costs = list(resources)
+                ordered_costs.sort(key=lambda x: resource_order[x[1]])
+            conversions[robot_type] = ordered_costs
         yield label, conversions
 
 
