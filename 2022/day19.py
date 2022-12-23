f4cb14fd073f96c03389e057df8fe67c65f8c613
Carl Waldbieser <cwaldbieser@gmail.com>
2022-12-22 21:46:48 -0500

    Added resource caps.
    Added resource caps.

--- 2022/day19.py
+++ 2022/day19.py
@@ -17,32 +17,71 @@ def main(args):
         robots = collections.Counter()
         robots["ore"] = 1
         resources = collections.Counter()
+        res_caps = calc_resource_caps(blueprint)
         results = evaluate_blueprint(
             time=1,
             max_time=max_time,
             blueprint=blueprint,
             robots=robots,
             resources=resources,
+            resource_caps=res_caps,
         )
         print(results)
         for _, resources in results:
             print(resources)
 
 
-def evaluate_blueprint(time, max_time, blueprint, robots, resources):
+def calc_resource_caps(blueprint):
+    """
+    Calculate the resource caps from the blueprint.
+    Since you can only produce a single robot each turn, the highest cost for a
+    resource in the blueprint is also the most that can be spent with that
+    resource in a single turn.  Accumulating extra resources is pointless.
+    """
+    caps = collections.Counter()
+    capped_resources = set(("ore", "clay", "obsidian"))
+    for costs in blueprint.values():
+        cost_map = collections.Counter(dict(costs))
+        for restype in capped_resources:
+            caps[restype] = max(caps[restype], cost_map[restype])
+    return caps
+
+
+def memoize(f):
+
+    cache = {}
+
+    def _inner(**kwds):
+        key = (
+            kwds["time"],
+            tuple(kwds["robots"].items()),
+            tuple(kwds["resources"].items()),
+        )
+        result = cache.get(key)
+        if result is None:
+            result = f(**kwds)
+            cache[key] = result
+            print("Stored key:", key)
+        return result
+
+    return _inner
+
+
+@memoize
+def evaluate_blueprint(time, max_time, blueprint, robots, resources, resource_caps):
     """
     Evaluate a blueprint.
     """
-    print(
-        "DEBUG time:",
-        time,
-        "max_time:",
-        max_time,
-        "robots:",
-        robots,
-        "resources:",
-        resources,
-    )
+    # print(
+    #     "DEBUG time:",
+    #     time,
+    #     "max_time:",
+    #     max_time,
+    #     "robots:",
+    #     robots,
+    #     "resources:",
+    #     resources,
+    # )
     if time == max_time:
         return [(robots, resources)]
     # Choices:
@@ -60,6 +99,10 @@ def evaluate_blueprint(time, max_time, blueprint, robots, resources):
             new_robots = collections.Counter(robots)
             new_resources = collections.Counter(resources) + new_allocations
         else:
+            cap = resource_caps[restype]
+            if cap != 0 and robots[restype] == cap:
+                # We've maxed out this resource production.
+                continue
             costs = blueprint[restype]
             can_produce = True
             for qty, res in costs:
@@ -68,14 +111,19 @@ def evaluate_blueprint(time, max_time, blueprint, robots, resources):
                     break
             if not can_produce:
                 continue
-            print("Building robot of type {}.".format(restype))
+            # print("Building robot of type {}.".format(restype))
             new_robots = robots + collections.Counter({restype: 1})
             res_adjustment = collections.Counter(dict((res, qty) for qty, res in costs))
-            print("res_adjustment:", res_adjustment)
+            # print("res_adjustment:", res_adjustment)
             new_resources = resources - res_adjustment + new_allocations
-            print("new_resources:", new_resources)
+            # print("new_resources:", new_resources)
         future_results = evaluate_blueprint(
-            time + 1, max_time, blueprint, new_robots, new_resources
+            time=time + 1,
+            max_time=max_time,
+            blueprint=blueprint,
+            robots=new_robots,
+            resources=new_resources,
+            resource_caps=resource_caps,
         )
         results.extend(future_results)
     return results
@@ -88,7 +136,7 @@ def harvest_resources(robots, resources):
     additional_resources = collections.Counter()
     for resource, qty in robots.items():
         additional_resources[resource] += qty
-        print("- Robots harvested {} units of {}.".format(qty, resource))
+        # print("- Robots harvested {} units of {}.".format(qty, resource))
     return additional_resources
 
 
